75ed2bfd6ddd546f690df191dc0f443dad2f6c0b
vlhells <vladislav5750@yandex.ru>
2021-10-28 22:06:16 +0300

    -First commit

---
+++ Homework3-4.Gladiator/Fighter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3_4.Gladiator
+{
+    class Fighter
+    {
+        int _hp, _damage;
+        string _name;
+        Random rnd = new Random();
+
+        public Fighter()
+        {
+            _name = Console.ReadLine();
+            _hp = rnd.Next(10,101);
+            _damage = rnd.Next(1, 21);
+            OutInfoAboutPers();
+        }
+
+        public void Fight(Fighter Attacked, Fighter Attacking)
+        {
+            MinusHp(Attacked, Attacking);
+            OutInfoAboutFight(Attacked, Attacking);
+        }
+        public int MinusHp(Fighter Attacked, Fighter Attacking)
+        {
+            Attacked._hp -= Attacking._damage;
+            return _hp;
+        }
+
+        void OutInfoAboutPers()
+        {
+            Console.WriteLine($"У {_name} {_hp} hp и {_damage} ед. урона.");
+        }
+
+        void OutInfoAboutFight(Fighter Attacked, Fighter Attacking)
+        {
+            Console.WriteLine($"{Attacking._name} ударил {Attacked._name} и нанёс ему {Attacking._damage} урона.");
+            Console.WriteLine($"Теперь у {Attacked._name} {Attacked._hp} hp.");
+        }
+    }
+}
---
+++ Homework3-4.Gladiator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Homework3_4.Gladiator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fighter fighter_1 = new Fighter();
+            Fighter fighter_2 = new Fighter();
+            int hp_1 = fighter_1.MinusHp(fighter_1, fighter_2);
+            int hp_2 = fighter_2.MinusHp(fighter_2, fighter_2);
+            do
+            {
+                fighter_1.Fight(fighter_1, fighter_2);
+                fighter_2.Fight(fighter_2, fighter_1);
+
+            }
+            while (hp_1 > 0 | hp_2 > 0);
+        }
+    }
+}
